661a1d3de6985e7dc9704f0ba876e341b79ad8ee
Saksham Verma <sakshamverma1000@gmail.com>
2025-04-09 10:44:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="font- text-4xl text-orange-400">
+      <h1>Awwards Project</h1>
+    </div>
+  );
+};
+
+export default App;
